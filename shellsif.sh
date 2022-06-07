dd67bef3c7ac7424e2e0903514f9461c02ee3ecf
templepmet <templepmet@gmail.com>
2022-06-08 04:36:18 +0900

    able to execute singularity shell

--- shellsif.sh
+++ shellsif.sh
@@ -4,6 +4,4 @@ source /etc/profile.d/lmod.sh
 # source /etc/profile.d/lmod.csh
 module load singularity
 # -fakeroot is not working
-# singularity build -f nest-gpu.sif nest-gpu.def
-singularity build nest-gpu.sif nest-gpu.def
 singularity shell --nv nest-gpu.sif
