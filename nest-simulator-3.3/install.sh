7587c210122436483faa3a8bb1a9adf9c8bc3b95
templepmet <templepmet@gmail.com>
2022-09-10 01:37:53 +0900

    easy to reinstall

--- nest-simulator-3.3/install.sh
+++ nest-simulator-3.3/install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -eu
 
-rm -rf build
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$NEST \
-	-Dwith-python=ON \
-	-Dwith-mpi=ON \
-	-Dwith-openmp=ON \
-	-Dwith-boost=ON \
-	-Dwith-readline=ON \
-	-Dwith-ltdl=ON \
-	-Dwith-gsl=ON \
-	..
+if [ -d build ]; then
+	cd build
+else
+	mkdir -p build
+	cd build
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$NEST \
+		-Dwith-python=ON \
+		-Dwith-mpi=ON \
+		-Dwith-openmp=ON \
+		-Dwith-boost=ON \
+		-Dwith-readline=ON \
+		-Dwith-ltdl=ON \
+		-Dwith-gsl=ON \
+		..
+fi
+
 make -j8
-rm -rf $NEST
 make install
