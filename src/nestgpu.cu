6ba718e46c81a57dcb71018a63507e6f42d3fcbc
templepmet <templepmet@gmail.com>
2022-09-25 02:48:51 +0900

    specific bug point of GetRecSpikeTimes

--- src/nestgpu.cu
+++ src/nestgpu.cu
@@ -1534,16 +1534,13 @@ int NESTGPU::GetNRecSpikeTimes(int i_node)
 int NESTGPU::GetRecSpikeTimes(int i_node, int n_node, int **n_spike_times_pt,
 			      float ***spike_times_pt)
 {
-  printf(" debug: before GetNodeSequenceOffset i_node=%d, n_node=%d, n_spike_times_pt=%x, spike_times_pt=%x\n", i_node, n_node, *n_spike_times_pt, *spike_times_pt);
   int i_group;
   int i_node_0 = GetNodeSequenceOffset(i_node, n_node, i_group);
-  printf(" debug: after GetNodeSequenceOffset i_node=%d, n_node=%d, n_spike_times_pt=%x, spike_times_pt=%x\n", i_node, n_node, *n_spike_times_pt, *spike_times_pt);
+  printf(" debug: i_node=%d, n_node=%d, i_group=%d, i_node_0=%d, ->n_node_=%d\n", i_node, n_node, i_group, i_node_0, node_vect_[i_group]->n_node_);
   if (i_node_0!=i_node || node_vect_[i_group]->n_node_!=n_node) {
-    printf(" debug: inner if");
     throw ngpu_exception("Spike times must be extracted for all and only "
 		  	  " the nodes of the same group");
   }
-  printf(" debug: before call node_vect_[i_group]->GetRecSpikeTimes\n");
   return node_vect_[i_group]->GetRecSpikeTimes(n_spike_times_pt,
 					       spike_times_pt);
 }
