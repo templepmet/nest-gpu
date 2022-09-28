cd5ec6c16c422a7e4f039caf04b0a82dce4f9301
templepmet <templepmet@gmail.com>
2022-09-28 22:13:48 +0900

    multi gpu arch

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eu
 
 autoreconf -i
-# ./configure --prefix=$NEST_GPU --with-gpu-arch=sm_75 # RTX 2080 Ti
-./configure --prefix=$NEST_GPU --with-gpu-arch=sm_61 # GTX 1080
+./configure --prefix=$NEST_GPU --with-gpu-arch=sm_61,sm_86 # GTX 1080, RTX A4500
 make -j24
 make install
