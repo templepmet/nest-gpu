a5e85fdf45fa12ba42fe5bc0ebe45f84bf519493
templepmet <templepmet@gmail.com>
2022-10-05 15:42:45 +0900

    success execute job on squid dbg queue

---
+++ ngpu_multi_area_model_simulation/multi-area-model-ngpu/job_hello.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#----- qsub option -----
+#PBS -q DBG
+#PBS --group=${GROUP_NAME}
+#PBS -l elapstim_req=00:10:00
+#PBS -o ./log/job_%s_out.txt
+#PBS -e ./log/job_%s_err.txt
+#PBS -b 1  # ノード数
+#PBS -l cpunum_job=76  # 1ノードあたりのCPUコア数 <=76
+#PBS -l gpunum_job=8   # 1ノードあたりのGPU使用台数 <=8
+#PBS -v OMP_NUM_THREADS=1 # 1ノードの（1プロセスあたりの？）スレッド並列実行数 <=76
+#PBS -T openmpi
+#PBS -v NQSV_MPI_MODULE=BaseGPU/2022
+#----- Program execution -----
+
+cd $PBS_O_WORKDIR
+
+module load BaseGPU/2022
+
+SINGULARITY_IMAGE=$WORK_DIR/nestgpu.sif
+PROGRAM=hellompi.py
+PROGRAM_SINGULARITY=`realpath $PROGRAM --relative-to=$HOME`
+RESULT_FILE=./log/result.txt
+
+if ls log/*.txt >/dev/null 2>&1
+then
+	mv log/*.txt log/old/
+fi
+
+date > $RESULT_FILE
+echo "PBS_JOBID: $PBS_JOBID" >> $RESULT_FILE
+
+time \
+	mpirun $NQSV_MPIOPTS -np 8 -npernode 8 --map-by core --bind-to core --display-devel-map \
+	singularity exec --nv $SINGULARITY_IMAGE python $PROGRAM_SINGULARITY \
+	>> $RESULT_FILE
