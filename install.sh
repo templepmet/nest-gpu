49190cde7a4f6cea39ea11a756f087e69cefe57a
templepmet <templepmet@gmail.com>
2022-10-26 14:51:24 +0900

    failed scorep-mpicxx

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -eu
 
+export MPICXX=mpicxx
+# alias scorep-mpicxx="scorep-mpicxx -no-pie"
+# export MPICXX=scorep-mpicxx
+
+./clean.sh
 autoreconf -i
 ./configure --prefix=$NEST_GPU --with-gpu-arch=sm_80 # for A100
 make -j24
